301284e138b5aabad5108c00822957620e8a276b
MayankChauhan7815 <109843573+MayankChauhan7815@users.noreply.github.com>
2022-10-12 16:35:17 +0530

    Update index.js

--- Dice Game ( Simple )/index.js
+++ Dice Game ( Simple )/index.js
@@ -2,14 +2,14 @@ var randomNumber = Math.floor(Math.random() * 6 + 1); // to generate random numb
 
 
 // for dice 1
-var imageSource1 = "images/dice" + randomNumber + ".png"; //to get random image of dice
+var imageSource1 = "Project-for-Fun/Dice Game ( Simple )/images/dice" + randomNumber + ".png"; //to get random image of dice
 var selector1 = document.querySelector(".dice .img1"); //selecting the dice
 var pageSource = selector1.setAttribute('src', imageSource1); // set image source in 'src' 
 
 
 // for dice 2
 var randomNumber2 = Math.floor(Math.random() * 6 + 1); 
-var imageSource2 = "images/dice" + randomNumber2 + ".png"; //to get random image of dice
+var imageSource2 = "Project-for-Fun/Dice Game ( Simple )/images/dice" + randomNumber2 + ".png"; //to get random image of dice
 var selector2 = document.querySelector(".dice .img2"); //selecting the dice
 var pageSource2 = selector2.setAttribute('src', imageSource2) // set image source in 'src' 
 
